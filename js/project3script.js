d45625c54a93fb9cfb5f4df02931f06ba7020faf
bkfwebdev <bryant.feld@gmail.com>
2016-11-16 22:07:46 -0500

    first commit

---
+++ js/project3script.js
@@ -0,0 +1,5 @@
+// on page loan set focus to the name input field
+$(document).ready(function(){$("#name").focus();}); 
+var jobRole = $("#title").val(); 
+var otherText = document.createElement("textarea");
+$("#title").change(function(){});
