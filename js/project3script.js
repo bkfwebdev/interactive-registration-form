a6eec81362df276277d81aaae999e60fb86b33aa
bkfwebdev <bryant.feld@gmail.com>
2016-11-18 15:01:18 -0500

    job title text field feature added

--- js/project3script.js
+++ js/project3script.js
@@ -1,5 +1,15 @@
 // on page loan set focus to the name input field
 $(document).ready(function(){$("#name").focus();}); 
-var jobRole = $("#title").val(); 
+// if job role is "other" add text area
 var otherText = document.createElement("textarea");
-$("#title").change(function(){});
+otherText.setAttribute("rows","1");
+otherText.setAttribute("cols","30");
+otherText.setAttribute("id","other-title")
+otherText.setAttribute("placeholder","Your Title");
+$("#title").change(function(){
+	var jobRole = $("#title").val();
+	console.log(jobRole);
+	if (jobRole === "other"){
+		$("fieldset")[0].append(otherText); 
+		}
+}); 
