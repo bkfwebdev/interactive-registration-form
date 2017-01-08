5195d5fdc03466978f7b098097bedb3efc0372d5
bkfwebdev <bryant.feld@gmail.com>
2017-01-07 22:44:10 -0500

    project complete version 1

--- js/project3scriptclean.js
+++ js/project3scriptclean.js
@@ -1,11 +1,110 @@
+//Bryant K. Feld Techdegree Project 3 - interactive registration form
+//global variables
+var colorSelectGuts = $("#color").html();
+var index1 = colorSelectGuts.indexOf('<option value="tomato">Tomato (I  ♥ JS shirt only)</option>');
+var index2 = colorSelectGuts.length - 1;
+var index3 = index1 - 1;
+var heartJS = colorSelectGuts.slice(index1,index2);
+var jsPuns = colorSelectGuts.slice(0,index3);
+var regTotal = 0;
+var checkboxToggle = [];
+var totalBox = document.createElement("p");
+var totalBoxText = "";
+var checkBoxes = $(".activities input[type='checkbox']" );
+var thePs =  $("p").parent("div");
+var creditCard = $("#credit-card");
+var creditCardValidator = {};
+var errorMessage = [];
+errorMessage[0] = "Name field can't be empty";
+errorMessage[1] = "Email field must be a validly formatted e-mail address";
+errorMessage[2] = "At least one activity must be selected";
+errorMessage[3] = "Payment option must be selected.";
+errorMessage[4] = "Make sure you have supplied a valid Credit Card number";
+errorMessage[5] = "Credit Card must have a valid zip code";
+errorMessage[6] = "Credit Card must have a valid 3 number CVV value.";
+var ok2Submit = false;
+var formCheck = [];
+//functions
+function validateEmail(email) 
+{
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
+function zipVal(zipCode)
+{
+    var re = /(^\d{5}$)|(^\d{5}-\d{4}$)/;
+    return re.test(zipCode);
+} 
+function cvvVal(cvv)
+{
+   var re = /^[0-9]{3}$/; 
+   return re.test(cvv);
+}
+function checkBasicInfo(boolArray){
+  
+ var boolresult = (boolArray[0] && boolArray[1] && boolArray[2]);
+    return boolresult;
+}
+
+function checkPaymentInfo(){
+    var cpibool = (($("#payment").val() === "paypal") || ($("#payment").val() === "bitcoin" ));
+    var ccbool1 = creditCardValidator.validate($("#cc-num").val());
+    var ccbool2 = zipVal($("#zip").val());
+    var ccbool3 = zipVal($("#zip").val());
+    if (cpibool === true){
+        return cpibool;
+    } else {
+    if ($("#payment").val() === "credit card" ){
+        return (ccbool1 && ccbool2 && ccbool3);
+                        }
+    }
+}
+function checkTheEngine(){
+    for (x = 0; x <=6 ; x++){
+        console.log("condition # " + x + " " + formCheck[x]);
+                }
+    console.log(ok2Submit);
+    console.log($("#payment").val());
+}
+//credit card object
+creditCardValidator.cards = {
+	'mc':'5[1-5][0-9]{14}',
+	'ec':'5[1-5][0-9]{14}',
+	'vi':'4(?:[0-9]{12}|[0-9]{15})',
+	'ax':'3[47][0-9]{13}',
+	'dc':'3(?:0[0-5][0-9]{11}|[68][0-9]{12})',
+	'bl':'3(?:0[0-5][0-9]{11}|[68][0-9]{12})',
+	'di':'6011[0-9]{12}',
+	'jcb':'(?:3[0-9]{15}|(2131|1800)[0-9]{11})',
+	'er':'2(?:014|149)[0-9]{11}'
+};
+
+creditCardValidator.validate = function(value,ccType) {
+	value = String(value).replace(/[- ]/g,''); //ignore dashes and whitespaces
+
+	var cardinfo = creditCardValidator.cards, results = [];
+	if(ccType){
+		var expr = '^' + cardinfo[ccType.toLowerCase()] + '$';
+		return expr ? !!value.match(expr) : false; // boolean
+	}
+
+	for(var p in cardinfo){
+		if(value.match('^' + cardinfo[p] + '$')){
+			results.push(p);
+		}
+	}
+	return results.length ? results.join('|') : false; // String | boolean
+};
+// main program logic
 // on page load set focus to the name input field & hide job role "other" text area
+// set initial shirt color to "select theme"
 $(document).ready(function(){
     $("#name").focus();
     $("#other-title").hide();
+    $("#color").append("<option value='select a t-shirt theme' selected>select a t-shirt theme</option>");
     
 }); 
 // if job role is "other" add text area
-
 $("#title").change(function(){
 	if ($("#title").val() === "other"){
       $("#other-title").show();
@@ -13,14 +112,7 @@ $("#title").change(function(){
             $("#other-title").hide();
         }
 }); 
-
 // hide invalid & show valid color select options for current t-shirt theme
-var colorSelectGuts = $("#color").html();
-var index1 = colorSelectGuts.indexOf('<option value="tomato">Tomato (I  ♥ JS shirt only)</option>');
-var index2 = colorSelectGuts.length - 1;
-var index3 = index1 - 1;
-var heartJS = colorSelectGuts.slice(index1,index2);
-var jsPuns = colorSelectGuts.slice(0,index3);
 $("#design").change (function(){
     if ($("#design").val() == "js puns"){
        $("#color").html(jsPuns);
@@ -29,20 +121,13 @@ $("#design").change (function(){
         $("#color").html(heartJS);
          } 
 });
-
-// dynamic itinerary update
-var regTotal = 0;
-var checkboxToggle = [];
+// dynamic itinerary update total registration cost and prevent conflicts
 for (x=0; x<=6; x++){
     checkboxToggle[x] = false;
 }
-var totalBox = document.createElement("p");
-var totalBoxText = "";
 totalBox.setAttribute("ID","totalBox");
 $(".activities").append(totalBox);
-var checkBoxes = $(".activities input[type='checkbox']" );
 checkBoxes.change(function(){
-
 var selected_id = Array.prototype.indexOf.call(checkBoxes, this);
 console.log(selected_id);
 if (selected_id === 0){
@@ -52,7 +137,7 @@ if (selected_id === 0){
     }else{
     if(checkboxToggle[0] === true){
         regTotal = regTotal - 200;
-        checkboxToggle[0] = false
+        checkboxToggle[0] = false;
     }
     }
 }
@@ -138,164 +223,58 @@ if (selected_id === 6){
        totalBoxText = "";
        totalBox.innerHTML = totalBoxText;
    }
-})
+});
 //hide/show active payment option (credit card as default)
 // grab paragraphs for bitcoin and pay pal
-var thePs =  $("p").parent("div");
 thePs[0].style.display = "none";
 thePs[1].style.display = "none";
-// grab credit card input div
-var creditCard = $("#credit-card");
+// dynamically change payment option section
 $("#payment").change(function(){
 // console.log($("#payment").val())
 if ($("#payment").val() == "bitcoin"){
-    // console.log("option 1");
-   $("p").parent("div")[0].style.display = "none";
-   $("p").parent("div")[1].style.display = "block";
+    $("p").parent("div")[0].style.display = "none";
+    $("p").parent("div")[1].style.display = "block";
    document.getElementById("credit-card").style.display = "none";
 } else {
-    // console.log("option 2");
-if ($("#payment").val() == "paypal"){
+  if ($("#payment").val() == "paypal"){
     $("p").parent("div")[0].style.display = "block";
     $("p").parent("div")[1].style.display = "none";
     document.getElementById("credit-card").style.display = "none";
 } else {
-    // console.log("option 3");
     $("p").parent("div")[0].style.display = "none";
     $("p").parent("div")[1].style.display = "none";
     document.getElementById("credit-card").style.display = "block";
 }
 }
 });
-// payment section & form validation
-// credit card validator
-// Create an object
-var creditCardValidator = {};
-// Pin the cards to them
-creditCardValidator.cards = {
-	'mc':'5[1-5][0-9]{14}',
-	'ec':'5[1-5][0-9]{14}',
-	'vi':'4(?:[0-9]{12}|[0-9]{15})',
-	'ax':'3[47][0-9]{13}',
-	'dc':'3(?:0[0-5][0-9]{11}|[68][0-9]{12})',
-	'bl':'3(?:0[0-5][0-9]{11}|[68][0-9]{12})',
-	'di':'6011[0-9]{12}',
-	'jcb':'(?:3[0-9]{15}|(2131|1800)[0-9]{11})',
-	'er':'2(?:014|149)[0-9]{11}'
-};
-// Add the card validator to them
-creditCardValidator.validate = function(value,ccType) {
-	value = String(value).replace(/[- ]/g,''); //ignore dashes and whitespaces
-
-	var cardinfo = creditCardValidator.cards, results = [];
-	if(ccType){
-		var expr = '^' + cardinfo[ccType.toLowerCase()] + '$';
-		return expr ? !!value.match(expr) : false; // boolean
-	}
-
-	for(var p in cardinfo){
-		if(value.match('^' + cardinfo[p] + '$')){
-			results.push(p);
-		}
-	}
-	return results.length ? results.join('|') : false; // String | boolean
-};
-function validateEmail(email) 
-{
-    var re = /\S+@\S+\.\S+/;
-    return re.test(email);
-}
-function zipVal(zipCode)
-{
-    var re = /(^\d{5}$)|(^\d{5}-\d{4}$)/;
-    return re.test(zipCode);
-} 
-function cvvVal(cvv)
-{
-   var re = /^[0-9]{3}$/; 
-   return re.test(cvv);
-}
-function checkBasicInfo(boolArray){
-  
- var boolresult = (boolArray[0] && boolArray[1] && boolArray[2]);
-    return boolresult;
-}
-
-function checkPaymentInfo(){
-    var cpibool = (($("#payment").val() === "paypal") || ($("#payment").val() === "bitcoin" ));
-    var ccbool1 = creditCardValidator.validate($("#cc-num").val());
-    var ccbool2 = zipVal($("#zip").val());
-    var ccbool3 = zipVal($("#zip").val());
-    if (cpibool === true){
-        return cpibool;
-    } else {
-    if ($("#payment").val() === "credit card" ){
-        return (ccbool1 && ccbool2 && ccbool3);
-                        }
-    }
-}
-function checkTheEngine(){
-    for (x = 0; x <=6 ; x++){
-        console.log("condition # " + x + " " + formCheck[x]);
-                }
-    console.log(ok2Submit3);
-    console.log($("#payment").val());
-}
-var errorMessage = [];
-var ok2Submit3 = false;
-errorMessage[0] = "Name field can't be empty"
-errorMessage[1] = "Email field must be a validly formatted e-mail address"
-errorMessage[2] = "At least one activity must be selected"
-errorMessage[3] = "Payment option must be selected." 
-// credit card conditions
-errorMessage[4] = "Make sure you have supplied a valid Credit Card number" 
-errorMessage[5] = "Credit Card must have a valid zip code" 
-errorMessage[6] = "Credit Card must have a valid 3 number CVV value."
-var formCheck = [];
+// initialize form check array and dynamically verify if ok to submit form and alert user of form errors
 for (y = 0; y <= 6; y++){formCheck[y] = false;}
 $("input").change(function(){
 // condition 0 - "Name field can't be empty"
-formCheck[0] = $("#name").val() != "";
+formCheck[0] = $("#name").val() !== "";
 // condition 1 - "Email field must be a validly formatted e-mail address"
 formCheck[1] = validateEmail($("#mail").val());
 // condition 2 - "At least one activity must be selected""
 formCheck[2] = regTotal > 0;
 // condition 3 - "Payment option must be selected." 
 formCheck[3] = ($("#payment").val() === "credit card" ) || ($("#payment").val() === "paypal") || ($("#payment").val() === "bitcoin" );
-// if payment type = PP or BC then payment ok = true ortherwise if payment type = cc payment ok = validate cc info
 // condition 4 - "Make sure you have supplied a valid Credit Card number" 
 formCheck[4] = creditCardValidator.validate($("#cc-num").val());
 // condition 5 - "Credit Card must have a valid zip code" 
 formCheck[5] = zipVal($("#zip").val());
 // condition 6 - "Credit Card must have a valid 3 number CVV value."
 formCheck[6] = cvvVal($("#cvv").val());
-var ok2Submit = checkBasicInfo(formCheck) && checkPaymentInfo();
-})
+});
 
 $("form").on("submit",function(e){
-    checkTheEngine();
+     ok2Submit = checkBasicInfo(formCheck) && checkPaymentInfo();
         if (ok2Submit === false){
             e.preventDefault();
             for (x=0; x<=6; x++){
                 if (formCheck[x] === false){
-                    alert(errorMessage[x])
+                    alert(errorMessage[x]);
                     break;
                 }
             }
         }
-    })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    });
